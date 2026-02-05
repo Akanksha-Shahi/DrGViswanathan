b056f475cfa6a6a708ae4e472f1766d8a791599d
Akanksha-Shahi <akankshashahi2005@gmail.com>
2026-02-05 10:52:51 +0530

    Add files via upload

---
+++ file9.cpp
@@ -0,0 +1,16 @@
+// MAXIMUM SUBARRAY
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int curSum = nums[0];
+        int maxSum = nums[0];
+        for( int i =1; i<n; i++){
+            curSum = max(nums[i], curSum +nums[i]);
+            maxSum = max(maxSum, curSum);
+        }
+        return maxSum;
+    }
+};

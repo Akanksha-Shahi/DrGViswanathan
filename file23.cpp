a58f1a64efdfb6e227bf42e4acbff886ed6d79d9
Akanksha-Shahi <akankshashahi2005@gmail.com>
2026-02-19 12:13:47 +0530

    Add files via upload

---
+++ file23.cpp
@@ -0,0 +1,14 @@
+// Is Subsequence
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i =0, j=0;
+        while( i< s.size() && j<t.size()){
+            if( s[i]== t[j]) i++;
+            j++;
+        }
+        return i == s.size();
+    }
+};

dc0bd88b9d1e08f7b32e01f694ba1ad3e87dc997
Akanksha Shahi <akankshashahi2005@gmail.com>
2026-02-23 10:22:19 +0530

    Add files via upload

---
+++ file27.cpp
@@ -0,0 +1,12 @@
+// VALID ANAGRAMS
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if( s.size()!= t.size()) return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};

c6c16182fe0cab290fe10ac29027d269471b7f74
Akanksha Shahi <akankshashahi2005@gmail.com>
2026-03-28 10:31:09 +0530

    Add files via upload

---
+++ file58.cpp
@@ -0,0 +1,17 @@
+// Container with most water
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left =0, right = height.size()-1, maxWater =0;
+        while(left<right){
+            int h = min(height[left], height[right]);
+            int width = right -left;
+            maxWater = max(maxWater, h*width);
+            if( height[left] < height[right]) left++;
+            else right--;
+        }
+        return maxWater;
+    }
+};

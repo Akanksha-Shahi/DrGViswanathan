5fc90c3926c2f7425853cf41c71f5bc8276e9cfe
Akanksha Shahi <akankshashahi2005@gmail.com>
2026-04-30 09:53:30 +0530

    Add files via upload

---
+++ file90.cpp
@@ -0,0 +1,19 @@
+// Integer Replacement
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int integerReplacement(int n) {
+       long long x =n;
+       int steps =0; 
+       while(x!=1){
+        if(x%2==0) x/=2;
+        else{
+            if(x==3 || (x&2) ==0) x--;
+            else x++;
+        }
+        steps++;
+       }
+       return steps;
+    }
+};
